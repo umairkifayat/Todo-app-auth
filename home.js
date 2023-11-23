06f49dc59816131682f1769fcb6f6410b7505149
umair kifayat <umairkifayat3@gmail.com>
2023-11-23 10:51:02 -0800

    'completed'

--- home.js
+++ home.js
@@ -1,142 +1,141 @@
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth, db } from "./config.js";
+import { collection, addDoc, getDocs, Timestamp, query, orderBy, deleteDoc, doc, updateDoc, where } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
-import {
-    onAuthStateChanged,
-    signOut
-  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-  import {
-    auth,
-    db
-  } from "./config.js";
-  import {
-    collection,
-    addDoc,
-    onSnapshot,
-    orderBy,
-    query,
-    Timestamp,
-    deleteDoc,
-    doc,
-    updateDoc
-  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
-  
-  const btn = document.querySelector('.signout');
-  const title = document.querySelector('.title');
-  const des = document.querySelector('.des');
-  const submit = document.querySelector('.submitbtn');
-  const div = document.querySelector('.render');
-  
-  let arr = [];
-  
-  // onauth functions start
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log(uid);
-    }
-  });
-  // onauth functions end
-  
-  // sign out function start
-  btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    signOut(auth).then(() => {
-      console.log('logout successfully');
-      window.location = './log.html';
-    }).catch((error) => {
-      console.log(error);
-    });
-  });
-  // sign out function end
-  
-  // render function start
-  function renderpost() {
-    div.innerHTML = '';
-    arr.forEach((item, index) => {
-      div.innerHTML += `
-        <div>
-          Title: ${item.Title}<br>
-          Description: ${item.Description}<br>
-          <button type="button" data-index="${index}" class="btn btn-danger text-white delete">Delete</button>
-          <button type="button" data-index="${index}" class="btn btn-info text-white update">Edit</button>
-        </div>
-        <hr>`;
-    });
-  
-    const delButtons = document.querySelectorAll('.delete');
-    const updButtons = document.querySelectorAll('.update');
-  
-    delButtons.forEach((btn) => {
-      btn.addEventListener('click', async (event) => {
-        const index = event.target.dataset.index;
-        console.log('delete called', arr[index]);
-        await deleteDoc(doc(db, 'posts', arr[index].Docid));
-      });
-    });
-  
-    updButtons.forEach((btn) => {
-      btn.addEventListener('click', async (event) => {
-        const index = event.target.dataset.index;
-        console.log('update called', arr[index]);
-        const updatedTitle = prompt('Enter new Title', arr[index].Title);
-        if (updatedTitle !== null) {
-          await updateDoc(doc(db, 'posts', arr[index].Docid), {
-            Title: updatedTitle
-          });
-        }
-      });
-    });
-  }
-  // render function end
-  
-  // get data from firestore start
-  async function getdatafirestore() {
-    const q = query(collection(db, "posts"), orderBy('PostDate', 'desc'));
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      arr = [];
-      querySnapshot.forEach((doc) => {
-        arr.push({
-          ...doc.data(),
-          Docid: doc.id
+
+const form = document.querySelector('.submitbtn');
+const title = document.querySelector('.title');
+const description = document.querySelector('.des');
+const card = document.querySelector('.render');
+const profileImage = document.querySelector('#image');
+const username = document.querySelector('#username');
+
+
+//user login or logout function
+onAuthStateChanged(auth, async (user) => {
+      if (user) {
+            const uid = user.uid;
+            const q = query(collection(db, "users"), where("uid", "==", uid));
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                  console.log(doc.data());
+            username.innerHTML = doc.data().nam
+            profileImage.src = doc.data().profileUrl
         });
+        getDataFromFirestore(user.uid)
+    } else {
+        window.location = 'index.html'
+    }
+});
+
+
+//logout function
+
+const logout = document.querySelector('.signout');
+
+logout.addEventListener('click', () => {
+      signOut(auth).then(() => {
+            console.log('logout successfully');
+            window.location = 'index.html'
+    }).catch((error) => {
+        console.log(error);
+    });
+})
+
+
+
+//get data from firestore
+
+let arr = [];
+
+function renderPost() {
+      card.innerHTML = ''
+      arr.map((item) => {
+            card.innerHTML += `
+            <div class="card mt-2">
+            <div class="card-body">
+                <p><span class="h4">Title:</span>${item.title}</p>
+                <p><span class="h4">Description:</span>${item.description}</p>
+                <button type="button" id="delete" class="btn btn-danger">Delete</button>
+                <button type="button" id="update" class="btn btn-info">Edit</button>
+            </div>
+    </div>`
+    })
+
+    const del = document.querySelectorAll('#delete');
+    const upd = document.querySelectorAll('#update');
+
+    del.forEach((btn, index) => {
+          btn.addEventListener('click', async () => {
+                console.log('delete called', arr[index]);
+                await deleteDoc(doc(db, "posts", arr[index].docId))
+                    .then(() => {
+                          console.log('post deleted');
+                          arr.splice(index, 1);
+                          renderPost()
+                      });
+              })
+    })
+    upd.forEach((btn, index) => {
+          btn.addEventListener('click', async () => {
+                console.log('update called', arr[index]);
+                const updatedTitle = prompt('enter new Title');
+            await updateDoc(doc(db, "posts", arr[index].docId), {
+                  title: updatedTitle
+              });
+              arr[index].title = updatedTitle;
+              renderPost()
+
+          })
+      })
+  }
+  
+async function getDataFromFirestore(uid) {
+    arr.length = 0;
+    const q = await query(collection(db, "posts"), orderBy('postDate', 'desc') , where('uid' , '==' , uid));
+    // const userUid = await auth.currentUser
+    console.log(uid);
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+          console.log(doc.data());
+          arr.push({ ...doc.data(), docId: doc.id });
       });
       console.log(arr);
-      renderpost();
-    });
-  
-    return unsubscribe;
+      renderPost();
   }
-  const unsubscribeFirestore = getdatafirestore();
-  // get data from firestore end
   
-  // add data on firestore function start
-  submit.addEventListener('click', async (event) => {
+  
+  
+  //post data on firestore
+  
+form.addEventListener('click', async (event) => {
     event.preventDefault();
     try {
-      const postObj = {
-        Title: title.value,
-        Description: des.value,
-        Uid: auth.currentUser.uid,
-        PostDate: Timestamp.fromDate(new Date())
-      };
-      await addDoc(collection(db, "posts"), postObj);
+        const postObj = {
+            title: title.value,
+            description: description.value,
+            uid: auth.currentUser.uid,
+            postDate: Timestamp.fromDate(new Date())
+        }
+        const docRef = await addDoc(collection(db, "posts"), postObj);
+        console.log("Document written with ID: ", docRef.id);
+        postObj.docId = docRef.id;
+        arr = [postObj, ...arr];
+        console.log(arr);
+        renderPost();
     } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  });
-  // add data on firestore function end
+          console.error("Error adding document: ", e);
+      }
+  })
   
-  // Unsubscribe from the Firestore listener when the page is unloaded
-  window.addEventListener('beforeunload', () => {
-    unsubscribeFirestore();
-  });
   
-
-
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
--- register.js
+++ register.js
@@ -1,9 +1,14 @@
-import { createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { auth } from "./config.js";
+import { createUserWithEmailAndPassword, } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth,db, storage } from "./config.js";
+import {collection, addDoc  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import {  ref, uploadBytes ,getDownloadURL  } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js";
+// import { ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js'
 
+const nam = document.querySelector('.username');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const btn = document.querySelector('.btn');
+const img = document.querySelector('.file');
 
 
 
@@ -18,7 +23,35 @@ event.preventDefault();
 createUserWithEmailAndPassword(auth, email.value, password.value)
   .then((userCredential) => {
     const user = userCredential.user;
-    console.log(user);
+    // console.log(user);
+    const storageRef = ref(storage, nam.value);
+
+// 'file' comes from the Blob or File API
+const file = img.files[0]
+
+uploadBytes(storageRef, file).then(() => {
+  getDownloadURL(storageRef(url))
+  .then((url) => {
+    addDoc(collection(db, "users"), {
+      name: nam.value,
+      email: email.value,
+      uid: user.uid,
+      profileUrl: url
+  }).then((res) => {
+      console.log(res);
+      window.location = 'log.html'
+  }).catch((err) => {
+      console.log(err);
+  })
+ 
+   
+  })
+  .catch((error) => {
+    // Handle any errors
+  });
+});
+
+    
     window.location = './home.html'
   })
   .catch((error) => {
